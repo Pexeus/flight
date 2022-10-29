d74ea51508750783635acdc4fbe31aebf11d4d56
Pexeus <liam.benedetti@gmx.ch>
2022-10-29 16:45:51 +0200

    restruc

---
+++ CGS/client/src/components/Floaters.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Map2d from "./Map2d"
+
+function Floaters({socket}) {
+  return (
+    <div className='floaterContainer'>
+        
+    </div>
+  )
+}
+
+export default Floaters
